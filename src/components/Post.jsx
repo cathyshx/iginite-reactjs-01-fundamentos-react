f5faace46916a292a08d52445fe435cf6afcbd0b
cathyshx <cacadasilva29@gmail.com>
2022-08-18 23:26:02 -0300

    Componente: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/cathyshx.png" />
+          <div className={styles.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="18 de Agosto às 23:25h" dateTime="2022-08-18 23:25:30">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+      <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
